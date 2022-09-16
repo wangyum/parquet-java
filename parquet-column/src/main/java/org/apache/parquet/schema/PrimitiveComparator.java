298eabb2b9523ca9c2f49fb0f2907be78adabf35
Yuming Wang <yumwang@ebay.com>
2022-09-17 07:59:07 +0800

    fix

--- parquet-column/src/main/java/org/apache/parquet/schema/PrimitiveComparator.java
+++ parquet-column/src/main/java/org/apache/parquet/schema/PrimitiveComparator.java
@@ -180,7 +180,7 @@ public abstract class PrimitiveComparator<T> implements Comparator<T>, Serializa
     }
   };
 
-  private static abstract class BinaryComparator extends PrimitiveComparator<Binary> implements Serializable {
+  private static abstract class BinaryComparator extends PrimitiveComparator<Binary> {
     @Override
     int compareNotNulls(Binary o1, Binary o2) {
       return compareBinary(o1, o2);
